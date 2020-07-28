6f146d9ad4af05298f62796fb8d5620dcef42945
Nicolaas Smit <nick.smit@rtk.io>
2020-07-28 06:37:32 +0200

    reformatting

--- core_components/dropdown.py
+++ core_components/dropdown.py
@@ -34,8 +34,8 @@ app.layout = html.Div([
     [Input('my-dropdown', 'value')])
 def update_output(value):
     NYC_img = enconde_image('https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fcincoveces.files.wordpress.com%2F2011%2F08%2Fnycpan2.jpg&f=1&nofb=1')
-    TX_img = enconde_image('https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.sparefoot.com%2Fmoving%2Fwp-content%2Fuploads%2F2015%2F12%2FThinkstockPhotos-480535456-1-1.jpg&f=1&nofb=1')
-    SF_img = enconde_image('https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.hotelgsanfrancisco.com%2Fassets%2Fthemes%2Fhotelgsanfrancisco%2Fimg%2FHotelG-map.jpg&f=1&nofb=1')
+    TX_img = enconde_image('https://www.houston.org/sites/default/files/2019-01/astros-stadium-houston.jpg')
+    SF_img = enconde_image('https://media.bizj.us/view/img/4473271/san-francisco*750xx2122-1196-0-186.jpg')
     if value == 'NYC':
         return html.Div(html.Img(src=NYC_img.decode(), style={'width': '500px', 'height':'400px'}))
     elif value == 'TX':
